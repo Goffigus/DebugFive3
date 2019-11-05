7897413238ec06311b1040ecc99b485f4b1c5e44
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-05 10:53:02 -0500

    Add project files.

---
+++ DebugFive3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+
+namespace DebugFive3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            int number;
+            const int LOW = 0;
+            const int HIGH = 99;
+            const int NUMINROW = 20;
+            for (letter = 'A'; letter <= 'Z'; ++letter)
+                for (number = LOW; number <= HIGH; ++number)
+                {
+                    if (number % NUMINROW == 0)
+                        WriteLine();
+                    Write("{0}{1} ", letter.ToString(), number.ToString("D2"));
+                }
+        }
+    }
+}
